544e12571d3692d12d8bbb9ab982f61667712dcf
khforever <eng.kholoudmohamed@gmail.com>
2024-01-05 01:26:00 +0200

    Enter Email Address or Your Mobile

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+
 
 class LoginController extends Controller
 {
@@ -37,4 +39,25 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+
+
+
+    public function credentials(Request $request){
+        if(is_numeric($request->email)){
+        return ['mobile'=>$request->email, 'password'=>$request->password];
+        }elseif(filter_var($request->email, FILTER_VALIDATE_EMAIL)){
+        return ['email'=>$request->email, 'password'=>$request->password];
+        }
+        }
+        
+
+
+
+
+
+
+
+
+
 }
