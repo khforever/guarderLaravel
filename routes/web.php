e0ae1764c167774a948839651b33fd9aa2204e13
khforever <eng.kholoudmohamed@gmail.com>
2024-01-22 23:47:16 +0200

    Task 15

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\GuardController;
+//use App\Http\Controllers\GuardController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GuardController;
 
 /*
 |--------------------------------------------------------------------------
